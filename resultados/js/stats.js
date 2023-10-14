a0578ba30c9dd87dc4dfe61c7747e5e5a9499f7e
Thiago Magro <thiago.magro@thiago.magro-Q6W6JF6KF7>
2023-10-14 18:08:38 -0300

    sync

---
+++ resultados/js/stats.js
@@ -0,0 +1,457 @@
+var stats = {
+    type: "GROUP",
+name: "All Requests",
+path: "",
+pathFormatted: "group_missing-name-b06d1",
+stats: {
+    "name": "All Requests",
+    "numberOfRequests": {
+        "total": "60003",
+        "ok": "60003",
+        "ko": "0"
+    },
+    "minResponseTime": {
+        "total": "0",
+        "ok": "0",
+        "ko": "-"
+    },
+    "maxResponseTime": {
+        "total": "226",
+        "ok": "226",
+        "ko": "-"
+    },
+    "meanResponseTime": {
+        "total": "4",
+        "ok": "4",
+        "ko": "-"
+    },
+    "standardDeviation": {
+        "total": "8",
+        "ok": "8",
+        "ko": "-"
+    },
+    "percentiles1": {
+        "total": "2",
+        "ok": "2",
+        "ko": "-"
+    },
+    "percentiles2": {
+        "total": "3",
+        "ok": "3",
+        "ko": "-"
+    },
+    "percentiles3": {
+        "total": "14",
+        "ok": "14",
+        "ko": "-"
+    },
+    "percentiles4": {
+        "total": "38",
+        "ok": "38",
+        "ko": "-"
+    },
+    "group1": {
+    "name": "t < 800 ms",
+    "htmlName": "t < 800 ms",
+    "count": 60003,
+    "percentage": 100
+},
+    "group2": {
+    "name": "800 ms <= t < 1200 ms",
+    "htmlName": "t >= 800 ms <br> t < 1200 ms",
+    "count": 0,
+    "percentage": 0
+},
+    "group3": {
+    "name": "t >= 1200 ms",
+    "htmlName": "t >= 1200 ms",
+    "count": 0,
+    "percentage": 0
+},
+    "group4": {
+    "name": "failed",
+    "htmlName": "failed",
+    "count": 0,
+    "percentage": 0
+},
+    "meanNumberOfRequestsPerSecond": {
+        "total": "291.277",
+        "ok": "291.277",
+        "ko": "-"
+    }
+},
+contents: {
+"req_busca-inva-lida-b5618": {
+        type: "REQUEST",
+        name: "busca inválida",
+path: "busca inválida",
+pathFormatted: "req_busca-inva-lida-b5618",
+stats: {
+    "name": "busca inválida",
+    "numberOfRequests": {
+        "total": "2390",
+        "ok": "2390",
+        "ko": "0"
+    },
+    "minResponseTime": {
+        "total": "0",
+        "ok": "0",
+        "ko": "-"
+    },
+    "maxResponseTime": {
+        "total": "163",
+        "ok": "163",
+        "ko": "-"
+    },
+    "meanResponseTime": {
+        "total": "4",
+        "ok": "4",
+        "ko": "-"
+    },
+    "standardDeviation": {
+        "total": "8",
+        "ok": "8",
+        "ko": "-"
+    },
+    "percentiles1": {
+        "total": "2",
+        "ok": "2",
+        "ko": "-"
+    },
+    "percentiles2": {
+        "total": "3",
+        "ok": "3",
+        "ko": "-"
+    },
+    "percentiles3": {
+        "total": "18",
+        "ok": "18",
+        "ko": "-"
+    },
+    "percentiles4": {
+        "total": "39",
+        "ok": "39",
+        "ko": "-"
+    },
+    "group1": {
+    "name": "t < 800 ms",
+    "htmlName": "t < 800 ms",
+    "count": 2390,
+    "percentage": 100
+},
+    "group2": {
+    "name": "800 ms <= t < 1200 ms",
+    "htmlName": "t >= 800 ms <br> t < 1200 ms",
+    "count": 0,
+    "percentage": 0
+},
+    "group3": {
+    "name": "t >= 1200 ms",
+    "htmlName": "t >= 1200 ms",
+    "count": 0,
+    "percentage": 0
+},
+    "group4": {
+    "name": "failed",
+    "htmlName": "failed",
+    "count": 0,
+    "percentage": 0
+},
+    "meanNumberOfRequestsPerSecond": {
+        "total": "11.602",
+        "ok": "11.602",
+        "ko": "-"
+    }
+}
+    },"req_busca-va-lida-d7f0d": {
+        type: "REQUEST",
+        name: "busca válida",
+path: "busca válida",
+pathFormatted: "req_busca-va-lida-d7f0d",
+stats: {
+    "name": "busca válida",
+    "numberOfRequests": {
+        "total": "5090",
+        "ok": "5090",
+        "ko": "0"
+    },
+    "minResponseTime": {
+        "total": "1",
+        "ok": "1",
+        "ko": "-"
+    },
+    "maxResponseTime": {
+        "total": "206",
+        "ok": "206",
+        "ko": "-"
+    },
+    "meanResponseTime": {
+        "total": "5",
+        "ok": "5",
+        "ko": "-"
+    },
+    "standardDeviation": {
+        "total": "9",
+        "ok": "9",
+        "ko": "-"
+    },
+    "percentiles1": {
+        "total": "2",
+        "ok": "2",
+        "ko": "-"
+    },
+    "percentiles2": {
+        "total": "4",
+        "ok": "4",
+        "ko": "-"
+    },
+    "percentiles3": {
+        "total": "17",
+        "ok": "17",
+        "ko": "-"
+    },
+    "percentiles4": {
+        "total": "39",
+        "ok": "39",
+        "ko": "-"
+    },
+    "group1": {
+    "name": "t < 800 ms",
+    "htmlName": "t < 800 ms",
+    "count": 5090,
+    "percentage": 100
+},
+    "group2": {
+    "name": "800 ms <= t < 1200 ms",
+    "htmlName": "t >= 800 ms <br> t < 1200 ms",
+    "count": 0,
+    "percentage": 0
+},
+    "group3": {
+    "name": "t >= 1200 ms",
+    "htmlName": "t >= 1200 ms",
+    "count": 0,
+    "percentage": 0
+},
+    "group4": {
+    "name": "failed",
+    "htmlName": "failed",
+    "count": 0,
+    "percentage": 0
+},
+    "meanNumberOfRequestsPerSecond": {
+        "total": "24.709",
+        "ok": "24.709",
+        "ko": "-"
+    }
+}
+    },"req_criac-a-o-ce502": {
+        type: "REQUEST",
+        name: "criação",
+path: "criação",
+pathFormatted: "req_criac-a-o-ce502",
+stats: {
+    "name": "criação",
+    "numberOfRequests": {
+        "total": "27617",
+        "ok": "27617",
+        "ko": "0"
+    },
+    "minResponseTime": {
+        "total": "0",
+        "ok": "0",
+        "ko": "-"
+    },
+    "maxResponseTime": {
+        "total": "226",
+        "ok": "226",
+        "ko": "-"
+    },
+    "meanResponseTime": {
+        "total": "5",
+        "ok": "5",
+        "ko": "-"
+    },
+    "standardDeviation": {
+        "total": "9",
+        "ok": "9",
+        "ko": "-"
+    },
+    "percentiles1": {
+        "total": "3",
+        "ok": "3",
+        "ko": "-"
+    },
+    "percentiles2": {
+        "total": "4",
+        "ok": "4",
+        "ko": "-"
+    },
+    "percentiles3": {
+        "total": "17",
+        "ok": "17",
+        "ko": "-"
+    },
+    "percentiles4": {
+        "total": "43",
+        "ok": "43",
+        "ko": "-"
+    },
+    "group1": {
+    "name": "t < 800 ms",
+    "htmlName": "t < 800 ms",
+    "count": 27617,
+    "percentage": 100
+},
+    "group2": {
+    "name": "800 ms <= t < 1200 ms",
+    "htmlName": "t >= 800 ms <br> t < 1200 ms",
+    "count": 0,
+    "percentage": 0
+},
+    "group3": {
+    "name": "t >= 1200 ms",
+    "htmlName": "t >= 1200 ms",
+    "count": 0,
+    "percentage": 0
+},
+    "group4": {
+    "name": "failed",
+    "htmlName": "failed",
+    "count": 0,
+    "percentage": 0
+},
+    "meanNumberOfRequestsPerSecond": {
+        "total": "134.063",
+        "ok": "134.063",
+        "ko": "-"
+    }
+}
+    },"req_consulta-5d76b": {
+        type: "REQUEST",
+        name: "consulta",
+path: "consulta",
+pathFormatted: "req_consulta-5d76b",
+stats: {
+    "name": "consulta",
+    "numberOfRequests": {
+        "total": "24906",
+        "ok": "24906",
+        "ko": "0"
+    },
+    "minResponseTime": {
+        "total": "0",
+        "ok": "0",
+        "ko": "-"
+    },
+    "maxResponseTime": {
+        "total": "62",
+        "ok": "62",
+        "ko": "-"
+    },
+    "meanResponseTime": {
+        "total": "3",
+        "ok": "3",
+        "ko": "-"
+    },
+    "standardDeviation": {
+        "total": "5",
+        "ok": "5",
+        "ko": "-"
+    },
+    "percentiles1": {
+        "total": "2",
+        "ok": "2",
+        "ko": "-"
+    },
+    "percentiles2": {
+        "total": "3",
+        "ok": "3",
+        "ko": "-"
+    },
+    "percentiles3": {
+        "total": "8",
+        "ok": "8",
+        "ko": "-"
+    },
+    "percentiles4": {
+        "total": "32",
+        "ok": "32",
+        "ko": "-"
+    },
+    "group1": {
+    "name": "t < 800 ms",
+    "htmlName": "t < 800 ms",
+    "count": 24906,
+    "percentage": 100
+},
+    "group2": {
+    "name": "800 ms <= t < 1200 ms",
+    "htmlName": "t >= 800 ms <br> t < 1200 ms",
+    "count": 0,
+    "percentage": 0
+},
+    "group3": {
+    "name": "t >= 1200 ms",
+    "htmlName": "t >= 1200 ms",
+    "count": 0,
+    "percentage": 0
+},
+    "group4": {
+    "name": "failed",
+    "htmlName": "failed",
+    "count": 0,
+    "percentage": 0
+},
+    "meanNumberOfRequestsPerSecond": {
+        "total": "120.903",
+        "ok": "120.903",
+        "ko": "-"
+    }
+}
+    }
+}
+
+}
+
+function fillStats(stat){
+    $("#numberOfRequests").append(stat.numberOfRequests.total);
+    $("#numberOfRequestsOK").append(stat.numberOfRequests.ok);
+    $("#numberOfRequestsKO").append(stat.numberOfRequests.ko);
+
+    $("#minResponseTime").append(stat.minResponseTime.total);
+    $("#minResponseTimeOK").append(stat.minResponseTime.ok);
+    $("#minResponseTimeKO").append(stat.minResponseTime.ko);
+
+    $("#maxResponseTime").append(stat.maxResponseTime.total);
+    $("#maxResponseTimeOK").append(stat.maxResponseTime.ok);
+    $("#maxResponseTimeKO").append(stat.maxResponseTime.ko);
+
+    $("#meanResponseTime").append(stat.meanResponseTime.total);
+    $("#meanResponseTimeOK").append(stat.meanResponseTime.ok);
+    $("#meanResponseTimeKO").append(stat.meanResponseTime.ko);
+
+    $("#standardDeviation").append(stat.standardDeviation.total);
+    $("#standardDeviationOK").append(stat.standardDeviation.ok);
+    $("#standardDeviationKO").append(stat.standardDeviation.ko);
+
+    $("#percentiles1").append(stat.percentiles1.total);
+    $("#percentiles1OK").append(stat.percentiles1.ok);
+    $("#percentiles1KO").append(stat.percentiles1.ko);
+
+    $("#percentiles2").append(stat.percentiles2.total);
+    $("#percentiles2OK").append(stat.percentiles2.ok);
+    $("#percentiles2KO").append(stat.percentiles2.ko);
+
+    $("#percentiles3").append(stat.percentiles3.total);
+    $("#percentiles3OK").append(stat.percentiles3.ok);
+    $("#percentiles3KO").append(stat.percentiles3.ko);
+
+    $("#percentiles4").append(stat.percentiles4.total);
+    $("#percentiles4OK").append(stat.percentiles4.ok);
+    $("#percentiles4KO").append(stat.percentiles4.ko);
+
+    $("#meanNumberOfRequestsPerSecond").append(stat.meanNumberOfRequestsPerSecond.total);
+    $("#meanNumberOfRequestsPerSecondOK").append(stat.meanNumberOfRequestsPerSecond.ok);
+    $("#meanNumberOfRequestsPerSecondKO").append(stat.meanNumberOfRequestsPerSecond.ko);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,187 @@
 import { Elysia } from "elysia";
+import { AggregateSteps, AggregateGroupByReducers, createClient, SchemaFieldTypes } from 'redis';
+import dayjs from 'dayjs';
+import z from "zod";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const pessoaSchema = z.object({
+  nome: z.string({
+    required_error: "Nome é obrigatório",
+    invalid_type_error: "Nome precisar ser string",
+  }).min(1,"Nome é obrigatório"),
+  apelido: z.string({
+    required_error: "Apelido é obrigatório",
+    invalid_type_error: "Apelido precisar ser string",
+  }).min(1,"Apelido é obrigatório"),
+  nascimento: z.number(),
+  stack: z.array(z.string({ invalid_type_error: "Stack precisar ser string" }))
+});
+
+let redis_host="localhost";
+if(process.env.NODE_ENV=="prod"){
+  redis_host="host.docker.internal";
+}
+
+console.log('REDIS_HOST');
+console.log(redis_host);
+
+const client = createClient({
+  socket: {
+    host: redis_host,
+    port: 6379
+  }
+});
+
+let special_chars = /[,.<>{}[\]"':;!@#\$%\^&*()\ -+=~]/g;
+function escape_string(str) {
+  return str.replace(special_chars, (el) => {
+    return `\\${el}`;
+  });
+}
+
+async function command(cmd_string) {
+  // console.log(cmd_string.split(' '));
+  console.log(await client.sendCommand(cmd_string.split(' ')));
+}
+
+async function command_json(cmd_string, json_string, cmd_string_end) {
+  cmd_string = cmd_string.split(" ");
+  // json_string = escape_string(json_string).split(" ");
+  cmd_string = cmd_string.concat(json_string);
+
+  if (cmd_string_end && cmd_string_end != "") {
+    cmd_string_end = escape_string(cmd_string_end).split(" ");
+    cmd_string.concat(cmd_string_end);
+  }
+  // console.log(cmd_string);
+  // console.log(cmd_string.concat(json_string, cmd_string_end));
+  client.sendCommand(cmd_string);
+  // str.replace(/[^a-zA-Z ]/g, "")
+}
+
+client.on('error', err => console.log('Redis Client Error', err));
+
+await client.connect();
+try {
+  console.log(await client.ft.info("pessoa_idx"))
+  console.log(await client.ft.dropIndex("pessoa_idx"))
+} catch (e) {
+  console.log(e);
+}
+
+try {
+  // command_json()
+  // command("FT.CREATE pessoa_idx ON JSON PREFIX 1 pessoa: SCHEMA $.id as id TEXT $.stack.* AS stack Text $.apelido AS apelido TEXT $.nome as nome TEXT $.nascimento as nascimento NUMERIC"); // 'OK'
+  let idx = await client.ft.create('pessoa_idx', {
+    '$.id': {
+      type: SchemaFieldTypes.TEXT,
+      AS: 'id'
+    },
+    '$.apelido': {
+      type: SchemaFieldTypes.TEXT,
+      AS: 'apelido'
+    },
+    '$.nome': {
+      type: SchemaFieldTypes.TEXT,
+      AS: 'nome'
+    },
+    '$.nascimento': {
+      type: SchemaFieldTypes.NUMERIC,
+      AS: 'nascimento'
+    }
+  }, {
+    ON: 'JSON',
+    PREFIX: 'pessoa:'
+  });
+  // console.log(idx);
+} catch (e) {
+  if (e.message === 'Index already exists') {
+    console.log('Index exists already, skipped creation.');
+  } else {
+    // Something went wrong, perhaps RediSearch isn't installed...
+    console.error(e);
+    process.exit(1);
+  }
+}
+
+
+const app = new Elysia()
+  .get("/", () => "Hello Elysia")
+
+app.get('/pessoas/:id', async ({ params: { id } }) => {
+  // let pessoa = await client.sAdd('apelidos', body.apelido);
+  console.log(`Get Pessoa ${id}`)
+  // f4c7888c-6966-11ee-8b7f-daa61c5665de
+  let pessoa = client.json.get(`pessoa:${id}`)
+  pessoa.nascimento = dayjs(pessoa.nascimento).format('YYYY-MM-DD');
+  return pessoa;
+})
+
+app.get('/contagem-pessoas', async () => {
+  console.log("Contagem")
+  let count = await client.sCard("apelidos");
+  return count
+});
+
+app.get('/pessoas', async ({ query, set }) => {
+  // console.log(query.t);
+  if(!query.t || query.t.trim()==""){
+    set.status = 400
+    return;
+  }
+  let results = await client.ft.search(
+    'pessoa_idx',
+    `${query.t}`
+  );
+  return results.documents.map((e)=>{
+    e.value.nascimento = dayjs(e.value.nascimento).format('YYYY-MM-DD');
+    return e.value
+  });
+})
+
+app.post('/pessoas', async ({ body, set }) => {
+  let pessoa = body;
+  pessoa.nascimento = dayjs(pessoa.nascimento).valueOf();
+
+  let result = pessoaSchema.safeParse(pessoa);
+  console.log(result.error)
+  if (!result.success) {
+    set.status = 422
+    return;
+  }
+
+  let exists = await client.sAdd('apelidos', pessoa.apelido);
+  if (exists == 0) {
+    set.status = 422
+    return;
+  }
+  pessoa.id = crypto.randomUUID();
+  let idx = "pessoa:" + pessoa.id;
+  console.log(pessoa)
+
+  // let ret = await command_json(`JSON.SET pessoa:${pessoa.id} .`, JSON.stringify(pessoa));
+
+  let ret = await client.json.set(idx, '$', {
+    "id": pessoa.id,
+    "nome": pessoa.nome,
+    "apelido": pessoa.apelido,
+    "nascimento": pessoa.nascimento,
+    "stack": pessoa.stack
+  })
+  // console.log(ret);
+  set.status = 201
+  set.headers = {
+    "location": `/pessoas/${pessoa.id}`
+  }
+  return;
+})
+
+
+app.listen(process.env.HTTP_PORT || 3000);
+
+// app.listen({
+//   port: parseInt(process.env.HTTP_PORT || '3000'),
+//   hostname: process.env.HTTP_HOST || '0.0.0.0',
+// });
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
